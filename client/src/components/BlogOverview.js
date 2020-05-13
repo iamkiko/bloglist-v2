0f2c7461ce34506388484f1068432177676829e2
Christos <constantinides.ch@gmail.com>
2020-05-13 17:08:52 +0200

    Style BlogOverview

--- client/src/components/BlogOverview.js
+++ client/src/components/BlogOverview.js
@@ -1,30 +1,17 @@
 import React from "react"
-import { Link } from "react-router-dom"
-import Container from "@material-ui/core/Container"
-import Typography from "@material-ui/core/Typography"
+import { BlogContainer, ParentContainer, StyledLink } from "./style"
 
 const BlogOverview = ({ blog }) => {
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    // border: "solid",
-    // borderWidth: 1,
-    marginBottom: 5,
-    backgroundColor: "#F8F8F8",
-  }
-
   return (
-    // <div style={blogStyle}>
-    <Container fixed>
+    <ParentContainer>
       <div>
-        <Typography component="div" style={blogStyle}>
-          <Link to={`blogs/${blog.id}`}>
+        <BlogContainer>
+          <StyledLink to={`blogs/${blog.id}`}>
             {blog.title} ({blog.author})
-          </Link>
-        </Typography>
+          </StyledLink>
+        </BlogContainer>
       </div>
-    </Container>
-    // </div>
+    </ParentContainer>
   )
 }
 
--- client/src/components/style.js
+++ client/src/components/style.js
@@ -1,1 +1,35 @@
-import styled, { css } from "styled-components"
+import styled, { createGlobalStyle, css } from "styled-components"
+import { Link } from "react-router-dom"
+import Container from "@material-ui/core/Container"
+
+createGlobalStyle`
+      @import url('https://fonts.googleapis.com/css?family=Ubuntu');
+  * {
+      font-family: 'Ubuntu', sans-serif;
+  }
+
+`
+
+export const BlogContainer = styled.div`
+  height: 32px;
+  border-radius: 5px;
+  background-color: #f0f0f8;
+  padding: 10px 10px 0 30px;
+  width: auto;
+  margin-bottom: 10px;
+  font-size: 20px;
+  font-family: Ubuntu, sans-serif;
+`
+
+export const ParentContainer = styled(Container)`
+  &:nth-child(1) {
+    padding-top: 25px;
+  }
+
+  &:nth-last-child(1) {
+    padding-bottom: 25px;
+  }
+`
+export const StyledLink = styled(Link)`
+  color: #0a558c;
+`
